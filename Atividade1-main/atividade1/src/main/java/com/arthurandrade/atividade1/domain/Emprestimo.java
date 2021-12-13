6f01b81f2d5e9938ef4785c19953c2a7ec5e4206
feleyr <fernanda.leyr@gmail.com>
2021-12-13 20:01:02 -0300

    teste

--- Atividade1-main/atividade1/src/main/java/com/arthurandrade/atividade1/domain/Emprestimo.java
+++ Atividade1-main/atividade1/src/main/java/com/arthurandrade/atividade1/domain/Emprestimo.java
@@ -1,5 +1,26 @@
 package com.arthurandrade.atividade1.domain;
+    import lombok.Data;
+    import lombok.NoArgsConstructor;
+    import lombok.AllArgsConstructor;
 
+    import javax.persistence.*;
+    import java.time.Instant;
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Entity(name = "table_emprestimo")
 public class Emprestimo {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", length = 64)
+    private String nome;
+
+    private Instant dataEmprestimo;
+    private Instant dataDevolucao;
+    private String nomePessoaDona;
+    private String nomePessoaTemporaria;
+    private String observacao;
 }
